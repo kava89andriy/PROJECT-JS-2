c43139115265cc543377bb5228985f5f92f3ee57
kava89andriy <110915269+kava89andriy@users.noreply.github.com>
2022-12-10 11:06:38 +0100

    project

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const convertCurrency = (quantity, code) => {
-  return fetch(
-    "https://api.nbp.pl/api/exchangerates/rates/c/" +
-      code +
-      "/today/?format=json"
-  )
+  return fetch(`https://api.nbp.pl/api/exchangerates/rates/c/${code}/`)
     .then((response) => response.json())
     .then((data) => {
-      let result = data.rates[0].bid * quantity;
+      const result = data.rates[0].bid * quantity;
 
-      document.getElementById("result").innerHTML = result.toFixed(2) + " PLN";
+      document.getElementById("result").innerHTML = `${result.toFixed(2)} PLN`;
     });
 };
 
-let convertBTN = document.getElementById("convert");
+const convertBTN = document.getElementById("convert");
 
 convertBTN.addEventListener("click", function () {
-  let currency = document.getElementById("currencies");
-  let currencyCode = currency.value;
-  let number = document.getElementById("number");
-  let quantity = number.value;
+  const currency = document.getElementById("currencies");
+  const currencyCode = currency.value;
+  const number = document.getElementById("number");
+  const quantity = number.value;
+  if (quantity > 0) {
+    convertCurrency(quantity, currencyCode);
+  } else alert("Value must be greater than or equal 0");
 
   convertCurrency(quantity, currencyCode);
 });
